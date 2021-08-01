b12a5a920137ce451201a073b2e71bbc4e8ca101
molzania <molzania_1507@yahoo.co.id>
2021-08-01 15:39:08 +0700

    commit on 'main'

---
+++ script.js
@@ -0,0 +1,20 @@
+//Soal 1//
+// Buat sebuah program untuk mengecek apakah karakter pertama dalam sebuah string mengandung huruf besar atau tidak.
+// Jika huruf pertama adalah huruf besar. Program akan menampilkan/console.log ‘Huruf pertama adalah huruf besar’
+// Jika huruf pertama bukan huruf besar. Program akan menampilkan/console.log ‘Huruf pertama adalah huruf kecil’
+
+let kalimat1 = "Jakarta";
+
+//Soal 2
+// Buat sebuah program untuk memvalidasi format nomor handphone
+// Nomor handphone minimum 10 digit dan maksimum 12 digit
+// Hanya menerima digit karakter
+
+//Soal 3 
+// Buat sebuah program yang menyediakan kalimat berikut sehingga user mendapatkan pencarian yang persis sama.
+// Bagaimana cara memulai usaha bisnis
+// Bootcamp impact byte
+// Status covid hari ini
+
+//Soal 4 
+//Buat tampilan untuk seluruh soal diatas. Buat sekreatif teman-teman. 1 soal = 1 fitur = 1 page.
