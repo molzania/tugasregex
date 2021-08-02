6cd6dfee89105a6119939eb6c63c94a07945d55e
molzania <molzania_1507@yahoo.co.id>
2021-08-02 11:56:06 +0700

    commit on 'main'

--- script.js
+++ script.js
@@ -21,16 +21,30 @@ function Soal1(kata) {
 // Nomor handphone minimum 10 digit dan maksimum 12 digit
 // Hanya menerima digit karakter
 
+// let regex2 = /^[0-9]{10,12}$/
+
+// function Soal2(number) {
+//     if(regex2.test(number) == true) {
+//         console.log("Terima kasih data sudah diterima");
+//     } else {
+//        console.log("Masukkan nomor hp hanya sebanyak 10-12 digit");
+//     }
+// } 
+
+// Soal2(0812856);
+
 let regex2 = /^[0-9]{10,12}$/
 
 function Soal2(number) {
-    let jawabanSoal2 = document.getElementById("jawaban-soal-2");
+    let jawabanSoal2 = document.getElementById("jawaban-soal-2").innerHTML;
     if(regex2.test(number) == true) {
-        jawabanSoal2.innerHTML = "Terima kasih data sudah diterima";
+        jawabanSoal2 = "Terima kasih data sudah diterima";
     } else {
-        jawabanSoal2.innerHTML = "Masukkan nomor hp hanya sebanyak 10-12 digit";
+       jawabanSoal2 = "Masukkan nomor hp hanya sebanyak 10-12 digit angka";
     }
 }
+
+
 
 //Soal 3 
 // Buat sebuah program yang menyediakan kalimat berikut sehingga user mendapatkan pencarian yang persis sama.
@@ -38,22 +52,26 @@ function Soal2(number) {
 // Bootcamp impact byte
 // Status covid hari ini
 
-let search = /^Bagaimana cara memulai usaha bisnis$|^Bootcamp impact byte$|^Status covid hari ini$/
-console.log(search.test("Status covid hari ini"));
+ let search = /^Bagaimana cara memulai usaha bisnis$|^Bootcamp impact byte$|^Status covid hari ini$/
+ console.log(search.test("Status covid hari ini"));
 
 function searchTest(kalimat) {
 
-    let 
-    if (search.test("Bagaimana cara memulai usaha bisnis")) {
-        console.log("Cara memulai usaha bisnis ialah..");
-    } if (search.test("Bootcamp impact byte")) {
-        console.log("Bootcamp impact byte adalah sebuah kegiatan...");
-    } if(search.test("Status covid hari ini")) {
-        console.log("Jumlah penderita Covid di Indonesia terus bertambah.");
-        
-    }
-}
+     if (search.test("Bagaimana cara memulai usaha bisnis")) {
+         console.log("Cara memulai usaha bisnis ialah..");
+         return true;
+     } if (search.test("Bootcamp impact byte")) {
+         console.log("Bootcamp impact byte adalah sebuah kegiatan...");
+         return true;
+     } if(search.test("Status covid hari ini")) {
+         console.log("Jumlah penderita Covid di Indonesia terus bertambah.");
+         return true;
+     }
+ }
 
 searchTest("Bagaimana cara memulai usaha bisnis");
+
+
+
 //Soal 4 
 //Buat tampilan untuk seluruh soal diatas. Buat sekreatif teman-teman. 1 soal = 1 fitur = 1 page.
